7087369dc9507b54ad61ef2a7c6594825935eb54
Sahil Sethi <113265897+SahilSethi5@users.noreply.github.com>
2023-11-10 22:00:18 -0500

    Initial Commit

---
+++ Coin Exchange Capstone iOS Development/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coin Exchange Capstone iOS Development
+//
+//  Created by Pawan Sethi on 11/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Coin Exchange Capstone iOS DevelopmentUITests/Coin_Exchange_Capstone_iOS_DevelopmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Coin_Exchange_Capstone_iOS_DevelopmentUITestsLaunchTests.swift
+//  Coin Exchange Capstone iOS DevelopmentUITests
+//
+//  Created by Pawan Sethi on 11/10/23.
+//
+
+import XCTest
+
+final class Coin_Exchange_Capstone_iOS_DevelopmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
